e027ffe65eb60212485249270ec2277d9e565e48
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-03-21 15:20:22 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// writeCards function
+function writeCards(namesArray, eventName) {
+    const cards = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+  }
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));  
+
+// write a function, countDown, that takes in any positive integer and, 
+// starting from that number, counts down to zero using console.log().
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
